67f6ce493a02ced7360c7b5a9ab13b03fdbdd5a6
Vo Le Huy Hoang <huyhoangvole@gmail.com>
2020-06-19 01:29:10 +0700

    mongodb

--- utils/MyConstants.js
+++ utils/MyConstants.js
@@ -2,7 +2,7 @@ var MyConstants = {
   DB_SERVER: 'cluster0-2vkmv.mongodb.net',
   DB_USER: 'huyhoangvole',
   DB_PASS: 'huyhoang.vole',
-  DB_DATABASE: 'shoppingonline',
+  DB_DATABASE: 'DoAn_NodeJS',
   HOSTNAME: 'localhost:3000',
   EMAIL_USER: 'hoang.187pm20475@vanlanguni.vn',
   EMAIL_PASS: 'HUYhoang2000',
