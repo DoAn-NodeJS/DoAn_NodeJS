efaa50f3272b1fbe319bf005f9a191b67ddd52c3
Vo Le Huy Hoang <huyhoangvole@gmail.com>
2020-06-22 04:46:12 +0700

    fix send mail

--- utils/MyConstants.js
+++ utils/MyConstants.js
@@ -3,7 +3,7 @@ var MyConstants = {
   DB_USER: 'nodejsk24vlu',
   DB_PASS: 'VLUnodejsk24',
   DB_DATABASE: 'DoAn_NodeJS',
-  HOSTNAME: 'localhost:3000',
+  HOSTNAME: 'localhost:5000',
   EMAIL_USER: 'nodejsk24vlu@gmail.com',
   EMAIL_PASS: 'VLUnodejsk24',
 };
